7ae9d64edd830ad1d281234175b1591bc7b9887d
PRAFUL <prafulpc045@gmail.com>
2016-03-01 17:06:00 +0530

    j

--- app/src/main/java/com/example/android/financialapp/FeedReaderContract.java
+++ app/src/main/java/com/example/android/financialapp/FeedReaderContract.java
@@ -15,5 +15,20 @@ public class FeedReaderContract {
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_SUBTITLE = "subtitle";
 
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String COMMA_SEP = ",";
+        private static final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                        FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                        FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                        FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+         // Any other options for the CREATE command
+                " )";
+
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
     }
+
+
 }
